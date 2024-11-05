9e21d00a0f9a3e68cd5e0c78a166e97447936d5d
Magratin <liupetery@gmail.com>
2024-11-05 09:38:57 +0100

    UserStory3

--- objects.js
+++ objects.js
@@ -12,4 +12,16 @@ const v = {
         return {v: 42}
     }
 }
-console.log(v.v().v);
+//console.log(v.v().v);
+
+//UserStory3
+function numberMembers(obj) {
+    return Object.keys(obj).length;
+}
+const person = {
+    name: 'John',
+    email: 'john@example.com',
+    password: 'password@example.com'
+}
+
+console.log(numberMembers(person))
