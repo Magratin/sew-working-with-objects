7b939ec296deb628a543f2dcd5e17bc48297655b
Magratin <liupetery@gmail.com>
2024-11-05 09:42:54 +0100

    UserStory4

--- objects.js
+++ objects.js
@@ -18,10 +18,25 @@ const v = {
 function numberMembers(obj) {
     return Object.keys(obj).length;
 }
+
 const person = {
     name: 'John',
     email: 'john@example.com',
     password: 'password@example.com'
 }
 
-console.log(numberMembers(person));
+
+//console.log(numberMembers(person));
+
+//UserStory4
+function equals(obj1, obj2) {
+    return obj1.name === obj2.name;
+}
+
+const person2 = {
+    email: 'john@example.com',
+    name: 'John',
+    password: 'password@example.com'
+}
+
+console.log(equals(person, person2));
